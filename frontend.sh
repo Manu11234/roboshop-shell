bd7fb5dea719c8d5fde532204d656002efde25e4
Manohar <manu.batchu@gmail.com>
2023-02-25 23:27:32 +0530

    roboshop components

--- frontend.sh
+++ frontend.sh
@@ -9,10 +9,10 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.z
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-echo -e "\e[35enabling reverse proxy\e[0m"
+echo -e "\e[35menabling reverse proxy\e[0m"
 cp configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 
-echo -e "\e[35enabling nginx\e[0m"
+echo -e "\e[35menabling nginx\e[0m"
 systemctl enable nginx
-echo -e "\e[35restarting nginx\e[0m"
+echo -e "\e[35mrestarting nginx\e[0m"
 systemctl restart nginx
