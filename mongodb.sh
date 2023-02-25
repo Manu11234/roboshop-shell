6758cac1d695ee4e892dfebfe731c92ba25993cf
Manohar <manu.batchu@gmail.com>
2023-02-25 16:46:51 +0530

    roboshop components

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,7 @@
-[mongodb-org-4.2]
-name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
-gpgcheck=0
-enabled=1
+cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+yum install mongodb-org -y
+systemctl enable mongod
+systemctl start mongod
+
+# update /etc/mongod.conf from 127.0.0.1 with 0.0.0.0
+
