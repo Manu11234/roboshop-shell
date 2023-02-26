cd49873a26f57704b5ab1ec13061b4b3bd1c1a49
Manohar <manu.batchu@gmail.com>
2023-02-26 21:03:48 +0530

    roboshop components

--- mongodb.sh
+++ mongodb.sh
@@ -5,6 +5,9 @@ cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo
 print_head "Install MongoDB"
 yum install mongodb-org -y &>>${log_file}
 
+print_head "Update Mongodb listen address"
+sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
+
 print_head "Enable MongoDB"
 systemctl enable mongod
 
