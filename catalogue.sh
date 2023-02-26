fd6b0b0b0d77bc94f28953b263b74e4198dcd458
Manohar <manu.batchu@gmail.com>
2023-02-26 21:32:28 +0530

    roboshop components

--- catalogue.sh
+++ catalogue.sh
@@ -1,44 +1,19 @@
 source common.sh
 
-print_head "downloading node js"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
-
-print_head "installing node js"
-yum install nodejs -y &>>${log_file}
-
-print_head "adding user"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
 useradd roboshop
 mkdir /app
-print_head "removing app old content"
-rm -rf /app/* &>>${log_file}
-
-print_head "downloading roboshop catalogue code"
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
-
+rm -rf /app/*
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
-
-print_head "unzip code"
-unzip /tmp/catalogue.zip &>>${log_file}
-
-print_head (update node js package)
-npm install &>>${log_file}
-
-print_head "accessing catalogue service"
-cp ${code_dir}/configs/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
-
+unzip /tmp/catalogue.zip
+cd /app
+npm install
+cp ${code_dir}/configs/catalogue.service /etc/systemd/system/catalogue.service
 systemctl daemon-reload
-
-print_head "enable catalogue"
-systemctl enable catalogue &>>${log_file}
-
-print_head "restart catalogue"
-systemctl start catalogue &>>${log_file}
-
-print_head "configure mongodb"
-cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
-
-print_head "Installing mongodb"
-yum install mongodb-org-shell -y &>>${log_file}
-
-print_head "Accessing schema"
-mongo --host mongodb.manudevops.tech </app/schema/catalogue.js &>>${log_file}
+systemctl enable catalogue
+systemctl start catalogue
+cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongodb.repo
+yum install mongodb-org-shell -y &
+mongo --host mongodb.manudevops.tech </app/schema/catalogue.js
