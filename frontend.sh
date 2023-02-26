782a86c475d01985ce369a6086637ee01440f2b1
Manohar <manu.batchu@gmail.com>
2023-02-26 17:19:02 +0530

    roboshop components

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,21 @@
 code_dir=$(pwd)
+log_file=tmp/roboshop.log
+rm -f ${log_file}
 echo -e "\e[35minstalling nginx\e[0m"
-yum install nginx -y
+yum install nginx -y &>>${log_file}
 
 echo -e "\e[35mremoving old content\e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>${log_file}
 
 echo -e "\e[35minstalling roboshop code\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
 echo -e "\e[35menabling reverse proxy\e[0m"
-cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
 
 echo -e "\e[35menabling nginx\e[0m"
-systemctl enable nginx
+systemctl enable nginx &>>${log_file}
 echo -e "\e[35mrestarting nginx\e[0m"
-systemctl restart nginx
+systemctl restart nginx &>>${log_file}
