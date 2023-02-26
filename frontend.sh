d6465889a3fa5084c7c5c4fdfe5b90c6303e0d35
Manohar <manu.batchu@gmail.com>
2023-02-26 18:11:31 +0530

    function practice

--- frontend.sh
+++ frontend.sh
@@ -1,21 +1,24 @@
 code_dir=$(pwd)
 log_file=/tmp/roboshop.log
 rm -f ${log_file}
-echo -e "\e[35minstalling nginx\e[0m"
+print_head (){
+  echo -e "\e[35m$1\e[0m"
+}
+print_head "installing nginx"
 yum install nginx -y &>>${log_file}
 
-echo -e "\e[35mremoving old content\e[0m"
+print_head "removing old content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
 
-echo -e "\e[35minstalling roboshop code\e[0m"
+print_head "installing roboshop code"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log_file}
 
-echo -e "\e[35menabling reverse proxy\e[0m"
+print_head "enabling reverse proxy"
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
 
-echo -e "\e[35menabling nginx\e[0m"
+print_head "enabling nginx"
 systemctl enable nginx &>>${log_file}
-echo -e "\e[35mrestarting nginx\e[0m"
+print_head "restarting nginx"
 systemctl restart nginx &>>${log_file}
