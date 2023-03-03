2262f3172369ee8e29a222b776c123060ce06a57
Manohar <manu.batchu@gmail.com>
2023-03-03 16:57:19 +0530

    roboshop components

--- frontend.sh
+++ frontend.sh
@@ -1,20 +1,27 @@
 source common.sh
 
 print_head "installing nginx"
-yum install nginx -y &>>${log_file}
+yum install nginx -y
+echo $?
 
 print_head "removing old content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
+echo $?
 
 print_head "installing roboshop code"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log_file}
+echo $?
 
 print_head "enabling reverse proxy"
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
+echo $?
 
 print_head "enabling nginx"
 systemctl enable nginx &>>${log_file}
+echo $?
+
 print_head "restarting nginx"
 systemctl restart nginx &>>${log_file}
+echo $?
